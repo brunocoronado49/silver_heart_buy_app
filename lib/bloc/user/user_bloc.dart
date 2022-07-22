49c44bc43da0b710dbe9f974dffebb2c0d940a35
Bruno Coronado <brunocoronado@MacBook-Air-de-Bruno.local>
2022-07-22 00:38:06 -0500

    Blocs creados

--- lib/bloc/user/user_bloc.dart
+++ lib/bloc/user/user_bloc.dart
@@ -1,4 +1,40 @@
+import 'dart:io';
+
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:silver_heart_buy/repository/repositories.dart';
+import 'package:silver_heart_buy/models/models.dart';
 
 part 'user_event.dart';
 part 'user_state.dart';
+
+class UserBloc extends Cubit<UserState> {
+  final UserRepository _repository;
+  late MyUser _user;
+  File? _image;
+
+  UserBloc(this._repository) : super(UserStateInitial());
+
+  void setImage(File? imagePicked) async {
+    _image = imagePicked;
+    emit(UserStateSuccess(_user, _image));
+  }
+
+  Future<void> getUser() async {
+    emit(UserStateLoading());
+    _user = (await _repository.getUsers()) ??
+    const MyUser('', '', '', '', '');
+
+    emit(UserStateSuccess(_user, _image));
+  }
+
+  Future<void> saveUser(String uid, String name, String description, String address, String email) async {
+    _user = MyUser(uid, name, description, address, email);
+    emit(UserStateSuccess(_user, _image, isSaving: true));
+    await Future.delayed(const Duration(seconds: 3));
+    await _repository.saveUser(_user, _image);
+    emit(UserStateSaved());
+    emit(UserStateSuccess(_user, _image, isSaving: false));
+  }
+}
--- lib/bloc/user/user_state.dart
+++ lib/bloc/user/user_state.dart
@@ -1,1 +1,23 @@
 part of 'user_bloc.dart';
+
+abstract class UserState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class UserStateInitial extends UserState {}
+
+class UserStateLoading extends UserState {}
+
+class UserStateSuccess extends UserState {
+  final MyUser user;
+  final File? image;
+  final bool isSaving;
+
+  UserStateSuccess(this.user, this.image, {this.isSaving = false});
+
+  @override
+  List<Object?> get props => [user, image?.path, isSaving];
+}
+
+class UserStateSaved extends UserState {}
